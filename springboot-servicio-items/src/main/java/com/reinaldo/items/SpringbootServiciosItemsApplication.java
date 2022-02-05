0a97333d8c191c2ee54af7c718aae1b9a19d8e6b
reinaldov95 <rv95850338@gmail.com>
2022-02-05 16:02:45 -0300

    microservicio Items

---
+++ springboot-servicio-items/src/main/java/com/reinaldo/items/SpringbootServiciosItemsApplication.java
@@ -0,0 +1,13 @@
+package com.reinaldo.items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiciosItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiciosItemsApplication.class, args);
+	}
+
+}
---
+++ springboot-servicio-items/src/test/java/com/reinaldo/items/SpringbootServiciosItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reinaldo.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiciosItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
